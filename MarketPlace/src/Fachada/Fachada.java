80e522e542ceaf5f483705ab2c54a651d55e7e6a
mateus-viana1 <134168622+mateus-viana1@users.noreply.github.com>
2023-05-30 14:49:54 -0300

    fachada at

--- MarketPlace/src/Fachada/Fachada.java
+++ MarketPlace/src/Fachada/Fachada.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 
 public class Fachada {
-    private ArrayList<Comprador> listaCompradores = new ArrayList<>();
-    private ArrayList<Loja> listaLojas = new ArrayList<>();
+    private static ArrayList<Comprador> listaCompradores = new ArrayList<>();
+    private static ArrayList<Loja> listaLojas = new ArrayList<>();
 	private static int id = 1; //Id para Lojas e compradores
 	private static ArrayList<Object> listaDeObjetos = new ArrayList<>();
 		
@@ -33,10 +33,10 @@ public class Fachada {
         }
 
         if(compradorAutenticado){ 
-            exibirMenuComprador(); // Incompleto
+           // exibirMenuComprador(); // Incompleto
         }
         else if(lojaAutenticada){
-            exibirMenuLoja(); // Incompleto
+          //  exibirMenuLoja(); // Incompleto
         }
         else{
             System.out.println("Usuário não cadastrado!!");
@@ -76,4 +76,22 @@ public class Fachada {
 		}
 	}//Fim do método recuperarID
 
-}
+    
+    private static void adicionarComprador(Comprador comprador) {
+    	listaCompradores.add(comprador);
+    }
+    
+    public static void cadastrarComprador(String nome, String email, String senha, String cpf, String endereco) {
+    	Comprador comprador = new Comprador(id, nome, email, senha, "Comprador", cpf, endereco);
+    	adicionarComprador(comprador);
+    }
+
+    private static void adicionarLojas(Loja loja) {
+        listaLojas.add(loja);
+    }
+
+    public static void cadastrarlojas(String nome, String email, String senha, String cnpj, String endereco) {
+        Loja lojas = new Loja(id, nome, email, senha, tipoUsuario:"Lojas", cnpj, endereco);
+        adicionarLojas(lojas);
+    }
+ }

76528d2410ff1f6270b9a1f400999167f7025855
mateus-viana1 <134168622+mateus-viana1@users.noreply.github.com>
2023-05-30 14:48:28 -0300

    menu mp

--- MarketPlace/src/MarketPlace.java
+++ MarketPlace/src/MarketPlace.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import Fachada.Fachada;
 import serializacao.Serializacao;
@@ -7,15 +8,41 @@ public class MarketPlace {
 	
 	public static void main(String[] args) {
 		
-		//Fachada fachada = new Fachada();
+		Fachada fachada = new Fachada();
+		Scanner sc = new Scanner(System.in);
 		
-		menu();
-		
+		menu(fachada, sc);
+		sc.close();
+	
 	}
 
-	private static void menu() {
-		// Criar o menu aqui e chamar fachada.cadastrarUsuario() ou fachada.autenticar etc...
+	private static void menu(Fachada fachada, Scanner sc) {
+		System.out.println("MENU");
+		System.out.println("1- Cadastrar comprador");
+		System.out.println("2- Cadastrar loja");
 		
+		cadastrarComprador();
+		cadastrarLoja();
+	}
+	
+	private static void cadastrarComprador() {
+		String nome = sc.nextLine();
+		String email = sc.nextLine();
+		String senha = sc.nextLine();
+		String cpf	= sc.nextLine();
+		String endereco = sc.nextLine();
+		fachada.cadastrarComprador(nome, email, senha, cpf, endereco);
+		fachada.listarCompradores();
+	}
+    
+	private static void cadastrarLoja() {
+		String nome = sc.nextLine();
+		String email = sc.nextLine();
+		String senha = sc.nextLine();
+		String cnpj	= sc.nextLine();
+		String endereco = sc.nextLine();
+		fachada.cadastrarLoja(nome, email, senha, cnpj, endereco);
+		fachada.listarLoja();
 	}
 
 }
